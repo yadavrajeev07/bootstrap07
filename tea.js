9b8b287c1536aa4c5ab0b0b2edad0c169f7376a1
Rajeev Kumar Yadav <rajeevyadavraju7@gmail.com>
2024-07-22 18:26:38 +0530

    Add files via upload

---
+++ tea.js
@@ -0,0 +1,11 @@
+function addNumbers(a, b) {
+    return a + b;
+  }
+  
+  // Using the function
+  let num1 = 5;
+  let num2 = 10;
+  let result = addNumbers(num1, num2);
+  
+  // Output the result
+  console.log("The sum is: " + result);
